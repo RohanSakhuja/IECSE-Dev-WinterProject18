342e46f7ef55254608d29e48f338bbbf28b8ddaf
AmeyaRele <rele.ameya@gmail.com>
2018-12-13 16:02:06 +0530

    Task1

--- task1/main.dart
+++ task1/main.dart
@@ -7,8 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generator',
-      theme: new ThemeData(          // Add the 3 lines from here... 
+      title: 'Changed',
+      theme: new ThemeData(           
         primaryColor: Colors.white,
       ), 
       home: RandomWords(),
