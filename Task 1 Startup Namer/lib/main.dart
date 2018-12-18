3865c7ecd8f0253cec97211ecf32f59d79b240ff
Royston <fernandesroy619@gmail.com>
2018-12-19 02:04:24 +0530

    Added main.dart

--- Task 1 Startup Namer/lib/main.dart
+++ Task 1 Startup Namer/lib/main.dart
@@ -1,109 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-        title: 'Startup Name Generator',
-        debugShowCheckedModeBanner: false,
-        theme: new ThemeData(
-            primaryColor: Colors.deepPurpleAccent
-        ),
-        home: new RandomWords()
+    
+    return MaterialApp(
+       title: 'Startup Name Generator',
+        home: RandomWords(),
+        
     );
   }
 }
-
 class RandomWords extends StatefulWidget {
   @override
-  createState() => new RandomWordsState();
+  RandomWordsState createState() => new RandomWordsState();
+}
+class RandomWordsState extends State<RandomWords> {
+  @override
+  
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: AppBar(
+      title: Text('Startup Name Generator'),
+    ),
+    body: _buildSuggestions(),
+  );
+  }
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  Widget _buildSuggestions() {
+  return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: /*1*/ (context, i) {
+        if (i.isOdd) return Divider(); /*2*/
+
+        final index = i ~/ 2; /*3*/
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+        }
+        return _buildRow(_suggestions[index]);
+      });
+}
+Widget _buildRow(WordPair pair) {
+  return ListTile(
+    title: Text(
+      pair.asPascalCase,
+      style: _biggerFont,
+    ),
+  );
 }
 
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = new Set<WordPair>();
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: new AppBar(
-            title: new Text('Startup Name Generator'),
-            actions: <Widget>[
-              new IconButton(icon: new Icon(Icons.list), onPressed: _pushsaved)
-            ]
-        ),
-        body: _buildSuggestions()
-    );
-  }
-
-  void _pushsaved() {
-    Navigator.of(context).push(
-        new MaterialPageRoute(
-          builder: (context) {
-            final tiles = _saved.map(
-                    (pair) {
-                  return new ListTile(
-                      title: new Text(
-                          pair.asPascalCase,
-                          style: _biggerFont
-                      )
-                  );
-                }
-            );
-            final divided = ListTile
-                .divideTiles(
-                tiles: tiles,
-                context: context
-            ).toList();
-
-            return new Scaffold(
-              appBar: new AppBar(
-                title: new Text('Saved Suggestions'),
-              ),
-              body: new ListView(children: divided),
-            );
-          },
-        )
-    );
-  }
-
-  Widget _buildRow(WordPair pair){
-    final alreadySaved = _saved.contains(pair);
-    return new ListTile(
-        title: new Text(
-          pair.asPascalCase,
-          style: _biggerFont,
-        ),
-        trailing: new Icon(
-            alreadySaved ? Icons.favorite : Icons.favorite_border,
-            color: alreadySaved? Colors.red : null
-        ),
-        onTap: () {
-          setState(() {
-            if(alreadySaved)
-              _saved.remove(pair);
-            else
-              _saved.add(pair);
-          });
-        }
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return new ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i){
-          if(i.isOdd) return new Divider();
-          final index = i ~/ 2;
-          if(index >= _suggestions.length){
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        }
-    );
-  }
 }
