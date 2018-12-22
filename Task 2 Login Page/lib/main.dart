a46d358b2fa36425be2f3d20e6acb7f6a96767b7
Royston <fernandesroy619@gmail.com>
2018-12-23 02:19:05 +0530

    Added Library folder and Screenshot of Task 2

--- Task 2 Login Page/lib/main.dart
+++ Task 2 Login Page/lib/main.dart
@@ -1,107 +1,122 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-      return new MaterialApp(
-        title: "Winter Project",
-        home: Scaffold(
-          body: LoginPage(),
-          ) ,
+  runApp(
+      MaterialApp(
         debugShowCheckedModeBanner: false,
-      );
-    }
-}
-
-class LoginPage extends StatefulWidget {
-  LoginPageState createState() => LoginPageState();
-}
-
-class LoginPageState extends State<LoginPage> {
-
-  final logo = new Hero(
-    tag: "logo",
-    child: Image.asset('logo.png'),
+        title: 'IECSE LOGIN',
+        home: Login(),
+      )
   );
+}
 
-  final email = TextFormField(
-      keyboardType: TextInputType.emailAddress,
-      autofocus: false,
-      decoration: InputDecoration(
-        hintText: 'Email',
-        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
-      ),
-    );
+class Login extends StatefulWidget {
 
-    final password = TextFormField(
-      autofocus: false,
-      obscureText: true,
-      decoration: InputDecoration(
-        hintText: 'Password',
-        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
-      ),
-    );
+  @override
+  State<StatefulWidget> createState() {
+
+    return _LoginState();
+  }
+}
+
+// ignore: expected_type_name
+class _LoginState extends State<Login> {
+
+  var _currencies = ['Rupees', 'Dollars', 'Pounds'];
+  final double  _minimumPadding = 5.0;
 
   @override
   Widget build(BuildContext context) {
-      return ListView(
-            padding: EdgeInsets.fromLTRB(40, 20, 40, 20),
-            children: <Widget>[
-              SizedBox(height: 30.0,),
-              logo,
-              SizedBox(height: 60.0,),
-              email,
-              SizedBox(height: 20.0,),
-              password,
-              SizedBox(height: 20.0,),
-              RaisedButton(
-                onPressed: (){
-                  Navigator.push(context, new MaterialPageRoute(builder: (context) => MyAnimation()));
-                },
-                color: Colors.lightBlueAccent,
-                elevation: 5.0,
-                padding: EdgeInsets.all(8.0),
-                shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
-                child: new Text('Log In', style: TextStyle(color: Colors.white)),
-              ),
-            ],
-          );
-    }
-}
 
-// Bonus Task Solution
-class MyAnimation extends StatefulWidget {
-  @override
-  MyAnimationState createState() => MyAnimationState();
-}
-
-class MyAnimationState extends State<MyAnimation>{
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
-      body: new Stack(
-        children:<Widget>[
-          new Center(
-            child: new Hero(
-              tag: "logo",
-              child: Image.asset('logo.png', width: 150, height: 150,),
-            ),
-          ),
-          Positioned(
-            top: 0.65*(MediaQuery.of(context).size.height),
-            left: 0.36*(MediaQuery.of(context).size.width),
-            child: SizedBox(
-              child: LinearProgressIndicator(),
-              width: 110.0,
-              height: 10.0,
-            ),
-          ),
-        ],
+//			resizeToAvoidBottomPadding: false,
+
+
+      body: Container(
+
+        margin: EdgeInsets.all(_minimumPadding * 5),
+        child: ListView(
+          children: <Widget>[
+
+            getImageAsset(),
+
+            Padding(
+                padding: EdgeInsets.only(top: _minimumPadding, bottom: _minimumPadding),
+                child: Center(
+                    child: Container(
+                        alignment: Alignment.center,
+                        width: 320.0,
+                        height: 35.0,
+                        child: TextFormField(
+                  decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(vertical: 10.0),
+                      labelText: '  EMAIL',
+                      hintText: '  Enter EMAIL ID',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30.0)
+                      )
+                  ),
+                )))),
+
+            Padding(
+                padding: EdgeInsets.only(top: _minimumPadding, bottom: _minimumPadding),
+                child: Center(
+                    child: Container(
+                        alignment: Alignment.center,
+                        width: 320.0,
+                        height: 50.0,
+                        child: TextFormField(
+                  decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(vertical: 10.0),
+                      labelText: '  PASSWORD',
+                      hintText: '  Enter Password',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30.0)
+                      )
+                  ),
+                )))),
+
+
+            Padding(
+                padding: EdgeInsets.only(bottom: _minimumPadding, top: _minimumPadding),
+                child: Row(children: <Widget>[
+                  Expanded(
+
+              child: Container(
+              alignment: Alignment.center,
+               width: 790.0,
+               height: 60.0,
+                    child: ButtonTheme(
+                      minWidth: 320.0,
+                      height: 40.0,
+                      child: RaisedButton(
+                      color: Colors.blue,
+                      // ignore: expected_token
+                      child:  Text('log in'),
+                        textColor: Colors.white,
+                      elevation: 10.0,
+                      shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+                      onPressed: () { print('Button Pressed');
+
+                      },
+                    ),
+                  ),
+
+
+             ))],)),
+
+          ],
+        ),
       ),
     );
-    }
+  }
+
+  Widget getImageAsset() {
+
+    AssetImage assetImage = AssetImage('assets/logo.png');
+    Image image = Image(image: assetImage, width: 145.0, height: 245.0,);
+
+    return Container(child: image, margin: EdgeInsets.all(_minimumPadding * 10),);
+  }
 }
